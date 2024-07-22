cae2987c72394c9d5ac31708e1a41b87740c2279
Siddhartha <siddharthapandit66@gmail.com>
2024-07-23 00:10:37 +0530

    added the login made a nav to display photo

--- register.js
+++ register.js
@@ -1,10 +1,9 @@
-
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    submitForm();
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  submitForm();
 });
 // async function submitForm() {
-   
+
 //     const name = document.getElementById("name").value;
 //     const email = document.getElementById("email").value;
 //     const password = document.getElementById("password").value;
@@ -61,42 +60,43 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
 //     }
 // }
 async function submitForm() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const phone = document.getElementById("phone").value;
-    const pan = document.getElementById("pantxt").value;
-    const aadhar = document.getElementById("aadhartxt").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const phone = document.getElementById("phone").value;
+  const pan = document.getElementById("pantxt").value;
+  const aadhar = document.getElementById("aadhartxt").value;
 
-    const aadharFile = document.getElementById("aadharfile").files[0];
-    const panFile = document.getElementById("panfile").files[0];
-    const photoFile = document.getElementById("photo").files[0];
+  const aadharFile = document.getElementById("aadharfile").files[0];
+  const panFile = document.getElementById("panfile").files[0];
+  const photoFile = document.getElementById("photo").files[0];
 
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("email", email);
-    formData.append("password", password);
-    formData.append("phone", phone);
-    formData.append("pan", pan);
-    formData.append("aadhar", aadhar);
-    formData.append("aadharimg", aadharFile);
-    formData.append("panimg", panFile);
-    formData.append("photo", photoFile);
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("password", password);
+  formData.append("phone", phone);
+  formData.append("pan", pan);
+  formData.append("aadhar", aadhar);
+  formData.append("aadharimg", aadharFile);
+  formData.append("panimg", panFile);
+  formData.append("photo", photoFile);
 
-    try {
-        const response = await fetch('http://127.0.0.1:8000/register/', {
-            method: 'POST',
-            body: formData,
-        });
+  try {
+    const response = await fetch("http://127.0.0.1:8000/register/", {
+      method: "POST",
+      body: formData,
+    });
 
-        if (response.ok) {
-            const responseData = await response.json();
-            console.log("Response from server", responseData);
-        } else {
-            const errorData = await response.json();
-            console.error("Validation errors", errorData);
-        }
-    } catch (error) {
-        console.error("Error during the fetch operation", error);
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log("Response from server", responseData);
+      window.location.href = "sucess.html";
+    } else {
+      const errorData = await response.json();
+      console.error("Validation errors", errorData);
     }
+  } catch (error) {
+    console.error("Error during the fetch operation", error);
+  }
 }
